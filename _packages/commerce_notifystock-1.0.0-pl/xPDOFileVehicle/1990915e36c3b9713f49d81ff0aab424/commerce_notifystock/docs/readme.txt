c8f513f96ab2345a5cd8fffa9eb45dc376f9e34b
IT-TONYPC\Tony <tonyklapatch@hotmail.com>
2020-10-24 09:29:56 -0400

    Remove accidentally committed files, update gitignore

--- _packages/commerce_notifystock-1.0.0-pl/xPDOFileVehicle/1990915e36c3b9713f49d81ff0aab424/commerce_notifystock/docs/readme.txt
+++
@@ -1,4 +0,0 @@
-NotifyStock for Commerce
-------------------------
-
-Allow customers to receive a notification when a product is back in stock.
--- _packages/commerce_notifystock-1.0.0-pl/xPDOFileVehicle/1990915e36c3b9713f49d81ff0aab424/commerce_notifystock/model/commerce_notifystock/metadata.mysql.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$xpdo_meta_map = array (
-  'comSimpleObject' => 
-  array (
-    0 => 'NotifyStockMessage',
-    1 => 'NotifyStockRequest',
-  ),
-);
